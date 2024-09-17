d27a3ef015d466c49649dad42c63e5d41fa366ae
MarcoParola <marcoparola96@gmail.com>
2024-09-17 15:18:18 +0200

    implement the method generating the forgetting subsets; there are 2 ways: by directly passing the list of element of the classes to forget or randomly

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import os
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks import ModelCheckpoint
-
+import torch
+import omegaconf
 
 def get_save_model_callback(save_path):
     """Returns a ModelCheckpoint callback
-    cfg: hydra config
+    
+    save_path: str: save path
     """
     save_model_callback = ModelCheckpoint(
         monitor='val_loss',
@@ -19,7 +21,8 @@ def get_save_model_callback(save_path):
 
 def get_early_stopping(patience=10):
     """Returns an EarlyStopping callback
-    cfg: hydra config
+
+    patience: int: patience
     """
     early_stopping_callback = EarlyStopping(
         monitor='val_loss',
@@ -27,5 +30,26 @@ def get_early_stopping(patience=10):
         patience=patience,
     )
     return early_stopping_callback
+
+# forgetting_set could be a list or a string
+def get_forgetting_subset(forgetting_set, n_classes, forgetting_set_size):
+    """Returns a forgetting subset
+
+    forgetting_set: str or list: forgetting set
+    n_classes: int: number of classes
+    forgetting_set_size: int: forgetting set size
+    """
+    
+    if forgetting_set == 'random':
+        # return random values using torch
+        return torch.randint(0, n_classes, (forgetting_set_size,)).tolist()
+    elif isinstance(forgetting_set, omegaconf.listconfig.ListConfig):
+        forgetting_set = list(forgetting_set)
+        return forgetting_set
+    else:
+        print('Unknown forgetting set')
+    return None
+    
+        
 
 
