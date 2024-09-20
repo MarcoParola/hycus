de87e4cb8d41d4de94b5978e57d71679b1a31763
MarcoParola <marcoparola96@gmail.com>
2024-09-20 16:24:11 +0200

    modify src/log.py in order to return a single logger (wandb logger) instead of a list of loggers, introduce train.py to train model on the full datasets

--- src/log.py
+++ src/log.py
@@ -9,14 +9,11 @@ def get_loggers(cfg):
     """Returns a list of loggers
     cfg: hydra config
     """
-    loggers = list()
-    if cfg.log.wandb:
-        from pytorch_lightning.loggers import WandbLogger
-        import wandb
-        hyperparameters = hp_from_cfg(cfg)
-        wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project)
-        wandb.config.update(hyperparameters)
-        wandb_logger = WandbLogger()
-        loggers.append(wandb_logger)
-
-    return loggers
+    
+    from pytorch_lightning.loggers import WandbLogger
+    import wandb
+    hyperparameters = hp_from_cfg(cfg)
+    wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project)
+    wandb.config.update(hyperparameters)
+    wandb_logger = WandbLogger()
+    return wandb_logger
---
+++ train.py
@@ -0,0 +1,83 @@
+import hydra
+import torch
+import os
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+import wandb
+from tqdm import tqdm
+
+#from src.utils import get_early_stopping, get_save_model_callback
+from src.models.resnet import ResNet, ResidualBlock
+from src.datasets.dataset import load_dataset
+from src.log import get_loggers
+from omegaconf import OmegaConf
+
+@hydra.main(config_path='config', config_name='config')
+def main(cfg):
+
+    # Set seed
+    if cfg.seed == -1:
+        random_data = os.urandom(4)
+        seed = int.from_bytes(random_data, byteorder="big")
+        cfg.seed = seed
+    torch.manual_seed(cfg.seed)    
+
+    wandb_logger = get_loggers(cfg) # loggers
+
+    # Load dataset
+    data_dir = os.path.join(cfg.currentDir, cfg.dataset.path)
+    train, val, test = load_dataset(cfg.dataset.name, data_dir, cfg.dataset.resize)
+    train_loader = DataLoader(train, batch_size=cfg.train.batch_size, shuffle=True, num_workers=cfg.train.num_workers)
+    val_loader = DataLoader(val, batch_size=cfg.train.batch_size, shuffle=False, num_workers=cfg.train.num_workers)
+    test_loader = DataLoader(test, batch_size=cfg.train.batch_size, shuffle=False, num_workers=cfg.train.num_workers)
+
+    model = ResNet(ResidualBlock, num_classes=cfg[cfg.dataset.name].n_classes)
+    model.to(cfg.device)
+
+    optimizer = AdamW(model.parameters(), lr=cfg.train.lr)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # Early stopping
+    #early_stopping = get_early_stopping(cfg)
+
+    for epoch in range(cfg.train.max_epochs):
+        model.train()
+        train_loss = 0
+        for i, (x, y) in enumerate(tqdm(train_loader)):
+            optimizer.zero_grad()
+            x = x.to(cfg.device)
+            y = y.to(cfg.device)
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
+            train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        train_loss /= len(train_loader)
+        wandb_logger.log_metrics({"train_loss": train_loss})
+
+        model.eval()
+        with torch.no_grad():
+            val_loss = 0
+            correct = 0
+            total = 0
+            for i, (x, y) in enumerate(tqdm(val_loader)):
+                x = x.to(cfg.device)
+                y = y.to(cfg.device)
+                y_pred = model(x)
+                loss = criterion(y_pred, y)
+                val_loss += loss.item()
+                _, predicted = y_pred.max(1)
+                total += y.size(0)
+                correct += predicted.eq(y).sum().item()
+
+            val_loss /= len(val_loader)
+            val_acc = 100 * correct / total
+
+            wandb_logger.log_metrics({"val_loss": val_loss, "val_acc": val_acc})
+
+            
+
+if __name__ == '__main__':
+    main()
+
